b5ea73cce649054f5ad7132d2cd5ebe2b6216ddb
Abhinav Sharma <abhi.sharm.414@gmail.com>
2020-04-15 20:27:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,10 @@
 def join_nested_strings(src)
   string = ''
-  src.each do |ele|
-    if ele.is_a?(String)
-      string += ele
-    end
+  src.each do |arr|
+    arr.each do |ele|
+      if ele.is_a?(String)
+        string += ele
+      end
   end
   string
 end
